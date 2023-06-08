3bd4d76283cc8b2da04064e8758a2f111cd26ca4
Kevin Huang <kevin.huang3@telus.com>
2023-06-08 17:33:46 -0400

    Initial Commit

---
+++ SwiftUITutorialOverlayDemo/SwiftUITutorialOverlayDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITutorialOverlayDemoApp.swift
+//  SwiftUITutorialOverlayDemo
+//
+//  Created by Kevin Huang on 6/8/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITutorialOverlayDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
